9b6d9f3be8ee1544d45280d77ce39a6d0fc4c265
skyairen <danyan96@gmail.com>
2016-11-18 00:19:51 -0500

    Revert "JSPs+Recipes.java"
    This reverts commit be3b80eaa81dab7532b9cad688883730e44c2256.

---
+++ mie350SampleProject/SimpleDBExample/src/com/mie/model/Ingredient.java
@@ -0,0 +1,162 @@
+package com.mie.model;
+
+public class Ingredient {
+	private int ingredientID;
+	private String name;
+	private int grams;
+	private int calories;
+	private double cholesterol;
+	private double sodium;
+	private double sugar;
+	private double fat;
+	private double protein;
+	private boolean gluten;
+	private boolean vA;
+	private boolean vB;
+	private boolean vC;
+	private boolean vD;
+	private boolean vE;
+	private boolean vK;
+	private boolean folicAcid;
+	private boolean calcium;
+	private boolean iron;
+	private boolean zinc;
+	private boolean potassium;
+	private boolean magnesium;
+
+	public int getIngredientID() {
+		return ingredientID;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getGrams() {
+		return grams;
+	}
+	public int getCalories() {
+		return calories;
+	}
+	public double getCholesterol() {
+		return cholesterol;
+	}
+	public double getSodium() {
+		return sodium;
+	}
+	public double getSugar() {
+		return sugar;
+	}
+	public double getFat() {
+		return fat;
+	}
+	public double getProtein() {
+		return protein;
+	}
+	public boolean getGluten() {
+		return gluten;
+	}
+	public boolean getVA() {
+		return vA;
+	}
+	public boolean getVB() {
+		return vB;
+	}
+	public boolean getVC() {
+		return vC;
+	}
+	public boolean getVD() {
+		return vD;
+	}
+	public boolean getVE() {
+		return vE;
+	}
+	public boolean getVK() {
+		return vK;
+	}
+	public boolean getFolicAcid() {
+		return folicAcid;
+	}
+	public boolean getCalcium() {
+		return calcium;
+	}
+	public boolean getIron() {
+		return iron;
+	}
+	public boolean getZinc() {
+		return zinc;
+	}
+	public boolean getPotassium() {
+		return potassium;
+	}
+	public boolean getMagnesium() {
+		return magnesium;
+	}
+	
+	public void setIngredientID(int ingID) {
+		this.ingredientID = ingID;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setGrams(int grams) {
+		this.grams = grams;
+	}
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
+	public void setCholesterol(double cholesterol) {
+		this.cholesterol = cholesterol;
+	}
+	public void setSodium(double sodium) {
+		this.sodium = sodium;
+	}
+	public void setSugar(double sugar) {
+		this.sugar = sugar;
+	}
+	public void setFat(double fat) {
+		this.fat = fat;
+	}
+	public void setProtein(double protein) {
+		this.protein = protein;
+	}
+	public void setGluten(boolean gluten) {
+		this.gluten = gluten;
+	}
+	public void setVA(boolean VA) {
+		this.vA = VA;
+	}
+	public void setVB(boolean VB) {
+		this.vB = VB;
+	}
+	public void setVC(boolean VC) {
+		this.vC = VC;
+	}
+	public void setVD(boolean VD) {
+		this.vD = VD;
+	}
+	public void setVE(boolean VE) {
+		this.vE = VE;
+	}
+	public void setVK(boolean VK) {
+		this.vK = VK;
+	}
+	public void setFolicAcid(boolean fAcid) {
+		this.folicAcid = fAcid;
+	}
+	public void setCalcium(boolean calcium) {
+		this.calcium = calcium;
+	}
+	public void setIron(boolean iron) {
+		this.iron = iron;
+	}
+	public void setZinc(boolean zinc) {
+		this.zinc = zinc;
+	}
+	public void setPotassium(boolean potassium) {
+		this.potassium = potassium;
+	}
+	public void setMagnesium(boolean magnesium) {
+		this.magnesium = magnesium;
+	}
+
+
+}
